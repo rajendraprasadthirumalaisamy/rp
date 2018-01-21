e5ad8761b40778bfba9ac92fccb37d21946737bd
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-01-21 14:33:41 +0530

    Rename v.c to 1v.c

---
+++ 1v.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+char s1[30],s2[30];
+int i=0,j=0;
+printf("enter the string");
+gets(S1);
+printf("enter the string");
+gets(S2);
+while(s1[i]!='\0')
+{
+i++;
+}
+while(s2[j]!='\0')
+{
+s1[i]=s2[j];
+j++;
+i++;}
+s1[i]!='\0';
+printf("concatenate string is %s",s1);
+}
+}
