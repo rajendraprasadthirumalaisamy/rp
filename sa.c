421c6e28ef3377a55509a1e103887f75fcfbbeca
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-01-21 13:32:18 +0530

    Create sa.c

---
+++ sa.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+char s[100];
+int countdigits,countnumeric,countspace;
+int counter;
+countdigits=countnumeric=countspace=0;
+printf("hello world.123');
+get(s);
+while
