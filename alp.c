a1a0e4a6f7e83eb30f9db406cdc87cafb8b791af
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-01-21 16:12:35 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char str1;
+printf("enter the string");
+gtes(str1);
+if((str1>a)&&(str1<z)||(str1>0)&&(str1<9))
+{
+printf("it contains alphabet and numbers");
+}
+else
+{
+printf("no numbers and alphabets");
+}}
