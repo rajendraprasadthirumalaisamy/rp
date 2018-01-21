9608b2141e43c6bd2abfdc26bea3eee9d57972cd
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-01-21 13:36:17 +0530

    Update a.c

--- a.c
+++ a.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-#include<ctype.h>
+
 #include<string.h>
-int main()
+void main()
 {
 int i; 
  int count=0;
@@ -16,5 +16,5 @@ count++;
 }
 }
 printf("count: %d ",count);
-return 0;
+
 }
