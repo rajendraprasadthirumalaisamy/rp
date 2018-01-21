6f0a0c035662c265733a5cec7709ce3f58027b14
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-01-21 16:23:05 +0530

    Update and rename fib.c to fibc.c

--- fibc.c
+++ fibc.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int main()
+void main()
 {
-    int i, n=5, t1 = 0, t2 = 1, t;
+    int i, n1=5, t1 = 0, t2 = 1, t;
 
  
 
-    for (i = 1; i <n; ++i)
+    for (i = 1; i <n1; ++i)
     {
         printf("%d, ", t1);
         t = t1 + t2;
