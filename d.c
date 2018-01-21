84423f93502bcc8ecc4a945ebbde9e755c54cd67
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-01-21 13:41:41 +0530

    Create d.c

---
+++ d.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int search(int arr[], int n, int x)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (arr[i] == x)
+return i;
+}
+return -1;
+} 
+int main()
+{
+int arr[100], x, t, n, i;
+scanf("%d", &t); 
+while (t--)
+{
+scanf("%d", &n); 
+for (i=0; i<n; i++)
+scanf("%d",&arr[i]);
+scanf("%d", &x);
+printf("%d\n", search(arr, n, x));
+} 
+return 0;
+}
