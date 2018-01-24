908a1dc8eebb4307e21ee90b8d7933a54a6c8cb2
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-01-24 16:43:24 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    scanf("%d",&n);
+m=n+1;
+    while(m!=0)
+    {
+        if(m%10==0)
+        {
+            printf("%d",m);
+            break;
+        }
+        m++;
+    }
+  
+}
