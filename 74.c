5f889e10d7add0ca01e824d2943d8ac04cb61f00
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-01-31 16:06:10 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+#include <math.h>
+ int main()
+{
+       float a=5.4, b=5.6;
+       printf("round of  %f is  %f\n", i, round(a));
+       printf("round of  %f is  %f\n", j, round(b));
+       return 0;
+}
+       
