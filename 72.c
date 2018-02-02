1ff6c3ef7d2fb5b2efc62242ffa8a218a886e0c8
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-02-02 14:25:25 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+int main()
+{
+    char s[20]="rAjA";int flag=0,i;
+    for(i=0;i<=10;i++)
+    {
+        if((s[i]=='a')||(s[i]=='e')||(s[i]=='i')||(s[i]=='o')||(s[i]=='u')||(s[i]=='A')||(s[i]=='E')||(s[i]=='I')||(s[i]=='O')||(s[i]=='U'))
+        {
+          
+          flag=1;
+        }
+        
+    }
+        if(flag==1)
+        {
+            printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+    
+}
