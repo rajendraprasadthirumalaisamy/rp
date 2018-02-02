54143ec6b569f2be766ca08d9491230cfbbcfe3d
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-02-02 14:13:23 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+     char b[10],a[20]="malayalam";
+    printf("%s\n",a);
+   strcpy(b,a);  printf("%s\n",b);
+     strrev(b);
+  printf("%s\n",c);
+  if( strcmp(a,b) == 0 )
+    printf("palindrom");
+    else 
+    printf("not a palindrom");
+    
+}
