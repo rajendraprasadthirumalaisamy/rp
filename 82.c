4e2f972448ebfbc8b223c0ac7ac04d0eaeafc5c0
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-02-07 16:20:14 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float a=3.1,b=6.6,c;
+    printf("enter the area\n");
+   scanf("%f",&a);
+    printf("enter the breath\n");
+    scanf("%f",&b);
+    c=a*b;
+    printf("%f",c);
+}
