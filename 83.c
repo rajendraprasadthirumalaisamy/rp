8d37d6fc70fa95442b4419d7be75caa230cf2201
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-02-07 16:29:26 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the val\n");
+   scanf("%d",&a);
+    printf("enter the val\n");
+    scanf("%d",&b);
+    c=a*b;
+    printf("%d\n",c);
+      c=a/b;
+    printf("%d\n",c);
+        c=a%b;
+        
+    printf("%d",c);
+}
