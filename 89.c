70aad2e84dd94c82637cc97e536ccd62d27c6822
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-02-09 14:47:55 +0530

    Update 89.c

--- 89.c
+++ 89.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 void main()
 {
-int temp,i,j,a[20];
-printf("enter the number");
-for(i=0;i<=5;i++)
-{
-scanf("%d",&a[i]);
-}
-for(i=0;i<=5;i++)
-{
-for(j=i+1;j<=5;j++)
-{
-if(a[i]>a[j])
-{
-temp=a[i];
-a[i]=a[j];
-a[j]=temp;
-}
-}
-}
-for(i=0;i<=5;i++)
-{
-printf("%d",a[i]);
-}
+    int a[10],i,j,temp;
+    printf("enter the number\n");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a);
+    }
+    for(i=0;i<5;i++)
+    {
+        for(j=i+1;j<5;j++)
+        {
+         if(a[i]>a[j])
+         {
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+         } 
+        }
+    }
+    for(i=0;i<5;i++)
+    {
+        printf("%d",a[i]);
+    }
 }
