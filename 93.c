de85ed64faa775f454c78398766c69f2e9f5b6ea
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-02-19 16:07:02 +0530

    Create 93.c

---
+++ 93.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    float celsius,kelvian=273.15;
+    printf("enter the temperature in celsius\n");
+    scanf("%f",&celsius);
+    printf("\n%f",kelvian+celsius);
+    
+}
