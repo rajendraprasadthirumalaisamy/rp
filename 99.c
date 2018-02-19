ffa1a9758434effe508725066fbe180eb2ad5ab9
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-02-19 16:27:31 +0530

    Create 99.c

---
+++ 99.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d;
+    printf("enter the number\n");
+    scanf("%d",&a);
+     printf("enter the number\n");
+    scanf("%d",&b);
+     printf("enter the number\n");
+    scanf("%d",&c);
+  d=(a*b)%c;
+  printf("the result is %d\n",d);  
+}
