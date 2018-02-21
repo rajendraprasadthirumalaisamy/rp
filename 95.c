293655b809f851505d84c8f9d6189d379ed898b0
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-02-21 12:49:54 +0200

    Create 95.c

---
+++ 95.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    float a,b,c,d;
+    printf("enter the princile\n");
+    scanf("%f",&a);
+     printf("enter the month\n");
+    scanf("%f",&b);
+     printf("enter the time\n");
+    scanf("%f",&c);
+    d=(a*b*c)/100;
+    printf("the simple interest %f\n",d);
+}
