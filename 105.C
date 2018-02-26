fe7de55d1436c4d71cd203b11f0c051268b94159
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-02-26 16:44:07 +0530

    Create 105.C

---
+++ 105.C
@@ -0,0 +1,97 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the choice");
+    scanf("%d",&a);
+    switch(a)
+    {
+     
+    case 1:
+    printf("A");
+ 
+        break;
+         
+    case 2: 
+        printf("B");
+        break;
+    case 3:
+        printf("C");
+        break;
+    case 4:
+    printf("D");
+    break;
+    case 5:
+    printf("E");
+    break;
+    case 6:
+    printf("F");
+    break;
+    case 8: 
+    printf("G");
+    break;
+    case 9:
+    printf("H");
+    break;
+    case 10:
+    printf("I");
+    break;
+    case 11:
+    printf("J");
+ 
+        break;
+         
+    case 12 :
+        printf("K");
+        break;
+    case 13:
+        printf("L");
+        break;
+    case 14:
+    printf("M");
+    break;
+    case 15:
+    printf("N");
+    break;
+    case 16:
+    printf("O");
+    break;
+    case 18:
+    printf("P");
+    break;
+    case 19:
+    printf("Q");
+    break;
+    case 20:
+    printf("R");
+    break;
+    case 21:
+    printf("S");
+ 
+        break;
+         
+    case 22 :
+        printf("T");
+        break;
+    case 23:
+        printf("U");
+        break;
+    case 24:
+    printf("V");
+    break;
+    case 25:
+    printf("W");
+    break;
+    case 26:
+    printf("X");
+    break;
+      case 27:
+    printf("Y");
+    break;
+    case 28:
+    printf("Z");
+    break;
+    default:
+    printf("EXIT");
+    }
+}
