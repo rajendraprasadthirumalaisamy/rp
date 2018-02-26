e50cf55a6f05fdf4a465b4a581620d533b332c19
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-02-26 15:59:32 +0530

    Create 107.c

---
+++ 107.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+
+
+{
+    int a,b,c,d;
+    printf("enter the number\n");
+    scanf("%d",&a);
+        printf("enter the number\n");
+    scanf("%d",&b);
+    printf("enter the number\n");
+    scanf("%d",&c);
+d=(a*b)/c;
+printf("the value is %d",d);
+}
