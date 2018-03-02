365b373ab7087f837abccaa236c2febc8b8cdb66
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-03-02 13:12:50 +0200

    Rename 1.c to 221.c

---
+++ 221.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+
+char str[10]="aabbcc";
+//printf("Enter the string:");
+//scanf("%s",str);
+
+printf("The string  reverse is:%s", strrev( str ));
+
+}
